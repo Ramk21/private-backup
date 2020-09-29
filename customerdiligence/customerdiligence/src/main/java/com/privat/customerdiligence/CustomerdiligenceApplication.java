9a494970ff84c3d028d4c879fd36b1dd58c4412e
Ramk21 <57636169+Ramk21@users.noreply.github.com>
2020-09-29 19:09:54 +0530

    Add files via upload

---
+++ customerdiligence/customerdiligence/src/main/java/com/privat/customerdiligence/CustomerdiligenceApplication.java
@@ -0,0 +1,13 @@
+package com.privat.customerdiligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerdiligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerdiligenceApplication.class, args);
+	}
+
+}
---
+++ customerdiligence/customerdiligence/src/test/java/com/privat/customerdiligence/CustomerdiligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privat.customerdiligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerdiligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
